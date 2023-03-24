1755e790893dc1a38f9a413d296049146af1173a
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-03-24 11:17:22 +0530

    Update corefunctionality.js

--- src/corefunctionality.js
+++ src/corefunctionality.js
@@ -1,27 +1,51 @@
 
-const fileInput = document.querySelector('input[type="file"]');
+const fileInput = document.querySelector('input[type="file"]');//calling file input 
 const audio = new Audio();
-const playBtn = document.querySelector("#playbutton");
-const progressEl = document.querySelector('input[type="range"]');
+const playBtn = document.querySelector("#playbutton");//play button
+const skipBtn = document.querySelector("#skipbutton");//skip button
+const progressEl = document.querySelector('input[type="range"]');//progress bar
 let mouseDownOnSlider = false;
 
+let fileList = [];
+let currentFileIndex = 0;
+
+
+//loading data
 audio.addEventListener("loadeddata", () => {
   progressEl.value = 0;
 });
 
+//time update
 audio.addEventListener("timeupdate", () => {
   if (!mouseDownOnSlider) {
     progressEl.value = audio.currentTime / audio.duration * 100;
   }
 });
 
+
 audio.addEventListener("ended", () => {
+  currentFileIndex++;
+  if (currentFileIndex >= fileList.length) {
+    currentFileIndex = 0;
+  }
+  loadAudioFile(fileList[currentFileIndex]);
   playBtn.textContent = "▶️";
 });
 
+//play and pause button functionality
 playBtn.addEventListener("click", () => {
   audio.paused ? audio.play() : audio.pause();
   playBtn.textContent = audio.paused ? "▶️" : "⏸️";
+});
+
+//skip button functionality
+skipBtn.addEventListener("click", () => {
+  currentFileIndex++;
+  if (currentFileIndex >= fileList.length) {
+    currentFileIndex = 0;
+  }
+  loadAudioFile(fileList[currentFileIndex]);
+  playBtn.textContent = "▶️";
 });
 
 progressEl.addEventListener("change", () => {
@@ -38,45 +62,22 @@ progressEl.addEventListener("mouseup", () => {
 });
 
 fileInput.addEventListener("change", () => {
-  const file = fileInput.files[0];
-  if (file) {
-    const url = URL.createObjectURL(file);
-    audio.src = url;
-    audio.load();
+  currentFileIndex = 0;
+  fileList = fileInput.files;
+  if (fileList.length > 0) {
+    loadAudioFile(fileList[currentFileIndex]);
   }
 });
 
-/*const fileURLToPath = document.querySelector('input[type="file"]');
-const audio = new Audio(fileURLToPath);
-const playBtn = document.querySelector("#playbutton");
-const progressEl = document.querySelector('input[type="range"]');
-let mouseDownOnSlider = false;
-
-audio.addEventListener("loadeddata", () => {
-progressEl.value = 0;
-});
-audio.addEventListener("timeupdate", () => {
-if (!mouseDownOnSlider) {
-progressEl.value = audio.currentTime / audio.duration * 100;
+function loadAudioFile(file) {
+  const url = URL.createObjectURL(file);
+  audio.src = url;
+  audio.load();
 }
-});
 
-audio.addEventListener("ended", () => {
-  playBtn.textContent = "▶️";
-});
-
-playBtn.addEventListener("click", () => {
-  audio.paused ? audio.play() : audio.pause();
-  playBtn.textContent = audio.paused ? "▶️" : "⏸️";
-});
-
-progressEl.addEventListener("change", () => {
-  const pct = progressEl.value / 100;
-  audio.currentTime = (audio.duration || 0) * pct;
-});
-progressEl.addEventListener("mousedown", () => {
-  mouseDownOnSlider = true;
-});
-progressEl.addEventListener("mouseup", () => {
-  mouseDownOnSlider = false;
-});*/
+function loadAudioFile(file) {
+  const url = URL.createObjectURL(file);
+  audio.src = url;
+  audio.load();
+  document.getElementById("current-file").textContent = file.name;
+}
